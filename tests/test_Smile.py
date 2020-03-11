aebcecbdb04f47e08be0e9101296ae6c48738690
CoMPaTech <info@compa.nl>
2020-03-11 14:54:40 +0000

    Added tests and testdata

--- tests/test_Smile.py
+++ tests/test_Smile.py
@@ -1,17 +1,152 @@
+"""Plugwise Home Assistant module."""
+
 import time
-import unittest
-import xml.etree.cElementTree as Et
+import pytest
+import pytest_asyncio
+import pytest_aiohttp
+
+import asyncio
+import aiohttp
 import os
 
-from Plugwise_Smile import Smile
+from lxml import etree
+
+from Plugwise_Smile.Smile import Smile
+
+async def setup_app():
+    global smile_type
+    if not smile_type:
+        return False
+    app = aiohttp.web.Application()
+    app.router.add_get('/core/appliances',smile_appliances)
+    app.router.add_get('/core/direct_objects',smile_direct_objects)
+    app.router.add_get('/core/domain_objects',smile_domain_objects)
+    app.router.add_get('/core/locations',smile_locations)
+    app.router.add_get('/core/modules',smile_modules)
+    return app
+
+async def smile_appliances(request):
+    global smile_type
+    f=open('tests/{}/core.appliances.xml'.format(smile_type),'r')
+    data=f.read()
+    f.close()
+    return aiohttp.web.Response(text=data)
+
+async def smile_direct_objects(request):
+    global smile_type
+    f=open('tests/{}/core.direct_objects.xml'.format(smile_type),'r')
+    data=f.read()
+    f.close()
+    return aiohttp.web.Response(text=data)
+
+async def smile_domain_objects(request):
+    global smile_type
+    f=open('tests/{}/core.domain_objects.xml'.format(smile_type),'r')
+    data=f.read()
+    f.close()
+    return aiohttp.web.Response(text=data)
+
+async def smile_locations(request):
+    global smile_type
+    f=open('tests/{}/core.locations.xml'.format(smile_type),'r')
+    data=f.read()
+    f.close()
+    return aiohttp.web.Response(text=data)
+
+async def smile_modules(request):
+    global smile_type
+    f=open('tests/{}/core.modules.xml'.format(smile_type),'r')
+    data=f.read()
+    f.close()
+    return aiohttp.web.Response(text=data)
+
+async def test_smile_modules(aiohttp_client, loop):
+    global smile_type
+    smile_type = 'anna'
+    app = aiohttp.web.Application()
+    app.router.add_get('/core/modules',smile_modules)
+    client = await aiohttp_client(app)
+    resp = await client.get('/core/modules')
+    assert resp.status == 200
+    text = await resp.text()
+    assert 'xml' in text
+
+@pytest.mark.asyncio
+async def connect():
+    global smile_type
+    if not smile_type:
+        return False
+    port =  aiohttp.test_utils.unused_port()
+
+    app = await setup_app()
+
+    server = aiohttp.test_utils.TestServer(app,port=port,scheme='http',host='127.0.0.1')
+    await server.start_server()
+
+    client = aiohttp.test_utils.TestClient(server)
+    websession = client.session
+
+    url = '{}://{}:{}/core/modules'.format(server.scheme,server.host,server.port)
+    resp = await websession.get(url)
+    assert resp.status == 200
+    text = await resp.text()
+    assert 'xml' in text
+    assert '<vendor_name>Plugwise</vendor_name>' in text
+
+    smile = Smile( host=server.host, password='abcdefgh', port=server.port, websession=websession)
+    assert smile._timeout == 20
+    assert smile._domain_objects == None
+
+    """Connect to the smile"""
+    connection = await smile.connect()
+    assert connection == True
+    return server,smile
 
 
-class TestSmileMethods(unittest.TestCase):
+@pytest.mark.asyncio
+async def list_devices(server,smile):
+    device_list={}
+    devices = smile.get_devices()
+    for dev in devices:
+        if dev['name'] == 'Controlled Device':
+            ctrl_id = dev['id']
+        else:
+            device_list[dev['id']]={'name': dev['name'], 'ctrl': ctrl_id}
+    #print(device_list)
+    return device_list
 
-    def setUp(self):
-        self.smile = Smile(
-            os.environ.get('SMILE_USERNAME', 'smile'), 
-            os.environ.get('SMILE_PASSWORD', 'short_id'),
-            os.environ.get('SMILE_IP', 'ip_address'),
-            os.environ.get('SMILE_PORT', 80)
-            )
+
+@pytest.mark.asyncio
+async def disconnect(server):
+    if not server:
+        return False
+    await server.close()
+
+@pytest.mark.asyncio
+async def test_connect_anna():
+    global smile_type
+    smile_type = 'anna'
+    server,smile = await connect()
+    device_list = await list_devices(server,smile)
+    #print(device_list)
+    for dev_id,details in device_list.items():
+        data = smile.get_device_data(dev_id, details['ctrl'])
+        assert data['type'] == 'thermostat'
+        assert data['setpoint_temp'] == 16.0
+        assert data['current_temp'] == 20.62
+        assert data['selected_schedule'] == 'Normal'
+        assert data['boiler_state'] == None
+        assert data['battery'] == None
+    await disconnect(server)
+
+@pytest.mark.asyncio
+async def test_connect_adam():
+    global smile_type
+    smile_type = 'adam'
+    server,smile = await connect()
+    device_list = await list_devices(server,smile)
+    print(device_list)
+    for dev_id,details in device_list.items():
+        data = smile.get_device_data(dev_id, details['ctrl'])
+        print(data)
+    await disconnect(server)

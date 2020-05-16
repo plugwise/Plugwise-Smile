2a8c41568d79f77d1bc3e0c514fae147b79f2625
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-05-16 11:25:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
+import codecs
+import os.path
 from setuptools import setup
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 def readme():
     with open("README.md") as f:
@@ -8,7 +22,7 @@ def readme():
 
 setup(
     name="Plugwise_Smile",
-    version="0.2.6",
+    version=get_version("Plugwise_Smile/__init__.py")
     description="Plugwise_Smile (Anna/Adam/P1) API to use in conjunction with Home Assistant.",
     long_description="Plugwise Smile API to use in conjunction with Home Assistant, but it can also be used without Home Assistant as a module.",
     keywords="HomeAssistant HA Home Assistant Anna Adam P1 Smile Plugwise",

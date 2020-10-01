50e1df6c92033770c0477e1c74a92bdcf5c553d0
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2020-10-01 20:16:03 +0200

    Update note

--- Plugwise_Smile/Smile.py
+++ Plugwise_Smile/Smile.py
@@ -752,7 +752,7 @@ class Smile:
             device_data.pop("boiler_state", None)
             device_data.pop("intended_boiler_state", None)
 
-        # Fix for Adam + Anna: intended_central_heating_state also present under Anna, don't use
+        # Fix for Adam + Anna: intended_central_heating_state also present under Anna, remove
         if "setpoint" in device_data:
             device_data.pop("heating_state", None)
 
